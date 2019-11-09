dddbf24b27c11bb5541f04eef1b4375c5e55207f
AustinSpinazze <47539055+AustinSpinazze@users.noreply.github.com>
2019-11-09 15:30:56 -0600

    Added Product Model and InMemory Product Repository Built

---
+++ MyShop/MyShop.Core/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.Models
+{
+    public class Product
+    {
+        public string productID { get; set; }
+        [StringLength(20)]
+        [DisplayName("Product Name")]
+        public string productName { get; set; }
+        public string productDescription { get; set; }
+        [Range(0, 1000)]
+        public string productPrice { get; set; }
+        public string productCategory { get; set; }
+        public string productImage { get; set; }
+        public Product()
+        {
+            this.productID = Guid.NewGuid().ToString();
+        }
+    }
+}
